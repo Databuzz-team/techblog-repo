b4974f6bfaefecfa0b2f7094c4cfee3214e82a6a
DanialDaeHyunNam <danialnam89@gmail.com>
2018-10-12 10:30:12 +0900

    Completed setup file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/bash/env bash
-mkdir scaffolds
-cp scaffolds_/draft.md scaffolds
-cp scaffolds_/page.md scaffolds
-cp scaffolds_/post.md scaffolds
-npm install
+isNew = "N"
+
+echo 본 파일은 두번 설치하면 안됩니다. 확실히 Databuzz tech blog를 처음 설치하시는거 맞나요[Y/N]?
+read isNew
+
+if [ ${isNew} = "Y" ] || [ ${isNew} = "y" ]; then
+  mkdir scaffolds
+  cp scaffolds_/draft.md scaffolds
+  cp scaffolds_/page.md scaffolds
+  cp scaffolds_/post.md scaffolds
+  npm install
+  cd themes
+  rm -rf hexo-theme-repo
+  git clone https://github.com/Databuzz-team/hexo-theme-repo.git
+  cd ..
+else
+  echo "Okay bye."
+fi
